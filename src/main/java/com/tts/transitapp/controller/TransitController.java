85a08b20521d19d3a07f828a7cf60cd0c15316e3
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-10 10:58:57 -0500

    Add initial getMapping method

--- src/main/java/com/tts/transitapp/controller/TransitController.java
+++ src/main/java/com/tts/transitapp/controller/TransitController.java
@@ -4,9 +4,16 @@ import com.tts.transitapp.service.TransitService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TransitController {
     @Autowired
-    TransitService transitService = new TransitService();
+    TransitService transitService;
+
+    @GetMapping("/")
+    public String methodName(){
+        transitService.run();
+        return "index";
+    }
 }
